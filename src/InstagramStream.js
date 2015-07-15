e1af3207ad73941d61eadd27b92c1b5a5522da1a
BOUCHER Tommy <contact@tommyboucher.com>
2015-07-15 18:09:03 +0200

    add bodyParser module

--- src/InstagramStream.js
+++ src/InstagramStream.js
@@ -1,11 +1,12 @@
 // External dependencies
 var connect = require('connect');
-var events  = require('events');
-var util    = require('util');
-var url     = require('url');
+var bodyParser = require('body-parser');
+var events = require('events');
+var util = require('util');
+var url = require('url');
 // Internal dependencies
 var SubscriptionManager = require('./SubscriptionManager.js');
-var MediaFetcher        = require('./MediaFetcher.js');
+var MediaFetcher = require('./MediaFetcher.js');
 
 // Inheritance
 util.inherits(InstagramStream, events.EventEmitter);
@@ -16,7 +17,7 @@ util.inherits(InstagramStream, events.EventEmitter);
  * @param {!Server} server  a Node HTTP server or Express App()
  * @param {object}  opts    options object
  */
-function InstagramStream (server, opts) {
+function InstagramStream(server, opts) {
   if (!(this instanceof InstagramStream)) {
     return new InstagramStream(server, opts);
   }
@@ -26,8 +27,8 @@ function InstagramStream (server, opts) {
 
   // Shift over parameters, if there is no server in the first slot
   if (typeof server === 'object' && typeof server.listen !== 'function') {
-    server  = null;
-    opts    = server;
+    server = null;
+    opts = server;
   }
 
   opts = opts || {};
@@ -47,56 +48,58 @@ function InstagramStream (server, opts) {
   }
 
   // Create object
-  this.client_id      = opts.client_id;
-  this.client_secret  = opts.client_secret;
-  this.callback_url   = opts.url + '/' + opts.callback_path;
-  this.url            = opts.url;
-  this.callback_path  = opts.callback_path;
+  this.client_id = opts.client_id;
+  this.client_secret = opts.client_secret;
+  this.callback_url = opts.url + '/' + opts.callback_path;
+  this.url = opts.url;
+  this.callback_path = opts.callback_path;
 
   this.last = {};
-  this.last.response  = undefined;
-  this.last.body      = undefined;
-  this.last_body      = undefined;    // TODO: Remove this
+  this.last.response = undefined;
+  this.last.body = undefined;
+  this.last_body = undefined; // TODO: Remove this
 
   // Defaults
   this.callback_path = this.callback_path ? this.callback_path : 'callback';
   var self = this;
 
   var _sub = new SubscriptionManager({
-    parent  : this,
-    client_id     : this.client_id,
-    client_secret : this.client_secret,
-    callback_url  : this.callback_url
+    parent: this,
+    client_id: this.client_id,
+    client_secret: this.client_secret,
+    callback_url: this.callback_url
   });
 
   var _fetch = new MediaFetcher({
-    parent  : this,
-    client_id     : this.client_id,
-    client_secret : this.client_secret,
-    callback_url  : this.callback_url
+    parent: this,
+    client_id: this.client_id,
+    client_secret: this.client_secret,
+    callback_url: this.callback_url
   });
 
   // Route '/:callback' -> custom handlers
-  ~function () {
+  ~ function () {
     var listeners = server.listeners('request');
 
     var chain = connect();
 
-    chain.use(connect.bodyParser());
+    chain.use(bodyParser());
 
     chain.use(function (req, resp, next) {
       var pathname = url.parse(req.url).pathname;
       if (req.method === 'GET' && pathname === '/' + self.callback_path) {
         var hub_challenge = url.parse(req.url, true).query['hub.challenge'];
-        resp.writeHead(200, { 'Content-Type': 'text/plain;charset=utf-8' });
+        resp.writeHead(200, {
+          'Content-Type': 'text/plain;charset=utf-8'
+        });
         resp.end(hub_challenge ? hub_challenge : '🍕');
-      }
-      else if (req.method === 'POST' && pathname === '/' + self.callback_path) {
-        resp.writeHead(200, { 'Content-Type': 'text/plain;charset=utf-8' });
+      } else if (req.method === 'POST' && pathname === '/' + self.callback_path) {
+        resp.writeHead(200, {
+          'Content-Type': 'text/plain;charset=utf-8'
+        });
         resp.end('🍕');
         route_traffic(req.body, req);
-      }
-      else {
+      } else {
         next();
       }
     });
@@ -131,23 +134,23 @@ function InstagramStream (server, opts) {
   };
 
   this.unsubscribe = function (id) {
-    id = id || 'all';
-    _sub.unsubscribe(id);
-  }
-  /**
-   * Request a Media Search from Instagram Based on an HTTP-Request
-   * ~~~
-   * @param {!Request Body} body an HTTP-request from the InstagramAPI containing
-   * object-id and subscription-id information. This can determine which type of
-   * subscription was sent, and what its purpose was.
-   */
-  function route_traffic (body) {
+      id = id || 'all';
+      _sub.unsubscribe(id);
+    }
+    /**
+     * Request a Media Search from Instagram Based on an HTTP-Request
+     * ~~~
+     * @param {!Request Body} body an HTTP-request from the InstagramAPI containing
+     * object-id and subscription-id information. This can determine which type of
+     * subscription was sent, and what its purpose was.
+     */
+  function route_traffic(body) {
     for (var k = 0; k < body.length; k++) {
       route_individual_media_response(body[k]);
     }
   }
 
-  function route_individual_media_response (result) {
+  function route_individual_media_response(result) {
     var sub_type = result.object;
     var sub_id = result.subscription_id;
     var obj_id = result.object_id;
